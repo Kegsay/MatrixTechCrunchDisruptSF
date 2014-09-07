677155ef2789512c95be405c8b99f6fa4b5b6a98
Erik Johnston <lightportal@gmail.com>
2014-09-07 08:45:27 +0100

    Do an initialSync.

--- src/org/matrix/techcrunch/matrix/EventStream.java
+++ src/org/matrix/techcrunch/matrix/EventStream.java
@@ -17,6 +17,8 @@ public class EventStream {
 	public EventStream(String host, String access_token, EventStreamCallback callback) {
 		this.callback = callback;
 		this.end_token = null;
+		this.host = host;
+		this.access_token = access_token;
 		this.base_url = host + MatrixUrls.PREFIX + MatrixUrls.EVENTS + "?access_token=" + access_token + "&timeout=30000";
 	}
 	
@@ -25,6 +27,7 @@ public class EventStream {
 			@Override
 			public void run() {
 				try {
+					doInitialSync();
 					do_stream();
 				} catch (IOException e) {
 					// TODO Auto-generated catch block
@@ -35,6 +38,46 @@ public class EventStream {
 				}
 			}
 		}, "EventStream").start();
+	}
+	
+	private void doInitialSync() throws IOException, JSONException {
+		String str_url = host + MatrixUrls.PREFIX + "/initialSync" + "?access_token=" + access_token;
+		
+		URL url = new URL(str_url);
+		HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+		
+		final int code;
+		final String resp;
+		try {
+			code = conn.getResponseCode();
+			BufferedInputStream in = new BufferedInputStream(conn.getInputStream());
+			BufferedReader streamReader = new BufferedReader(new InputStreamReader(in, "UTF-8")); 
+		    StringBuilder responseStrBuilder = new StringBuilder();
+		    String inputStr;
+		    while ((inputStr = streamReader.readLine()) != null)
+		        responseStrBuilder.append(inputStr);
+		    resp = responseStrBuilder.toString();
+		} finally {
+			conn.disconnect();
+		}
+		
+		if (code == 200) {
+			Log.i("test", "got response " + resp);
+			JSONObject json = new JSONObject(resp);
+			JSONArray rooms = json.getJSONArray("rooms");
+			for (int j = 0; j < rooms.length(); ++j) {
+				JSONArray chunks = rooms.getJSONObject(j).getJSONObject("messages").getJSONArray("chunk");
+				for (int i = 0; i < chunks.length(); ++i) {
+					try {
+						Event ev = Event.from_json(chunks.getJSONObject(i));
+						this.callback.onEvent(ev);
+					} catch(Exception e) {}
+				}
+			}
+			this.end_token = json.getString("end");
+		} else {
+			Log.e("test", "got response code: " + code);
+		}
 	}
 	
 	private void do_stream() throws IOException, JSONException {
@@ -86,4 +129,6 @@ public class EventStream {
 	private String end_token; 
 	private final EventStreamCallback callback;
 	private final String base_url;
+	private final String host;
+	private final String access_token;
 }
